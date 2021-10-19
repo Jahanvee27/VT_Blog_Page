6d1e0e7a83430ee178494a6d14a09216dfa2f615
Nivas Manduva <53264470+eviltypha@users.noreply.github.com>
2021-10-19 17:23:14 +0530

    Changed structure and ambience of webpage

--- VT_blogPage/jQuery/blog_page.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".more").hide();
-    $(".show_hide").click(function(){
-        event.stopImmediatePropagation();
-        var index = $(".show_hide").index(this);
-        $(this).text($(this).text() == 'Read More' ? 'Read Less' : 'Read More'); 
-        $(".more").eq(index).toggle();
-    });
-  });
